59339052de47c93f684e7dd5d6c3b8f4c4a24126
r2_bean2 <deanwm30@gmail.com>
2026-07-06 10:59:15 -0400

    adding the script part 2

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 import questionary
+import os
 thing_list = [
     "hi"
 ]
+file_path = "feedback.txt"
 while True:
     action = questionary.select(
         "Select list operation to perform:",
@@ -45,5 +47,26 @@ while True:
             ],
             ).ask()
         if yn == "no":
-            print("you dont want to leave feedback so you must not be sirouse about leaving")
+            print("you dont want to leave feedback so you must not be ready to leave")
+        else:
+            stuff = input("please explain your experince, and weather you'd use this script agian: ")
+            print("other respouses include:")
+
+            # Determine where this script is
+            full_path = __file__
+            if not os.path.abspath(full_path):
+                full_path = os.path.join(os.getcwd(), full_path)
+
+            # The directory it's located in should be install/local/
+             # So we should change to that
+            install_local_dir = os.path.dirname(full_path)
+            os.chdir(install_local_dir)
+            if not os.path.exists(file_path):
+                with open(file_path, "w") as file:
+                    pass  # 'pass' creates an empty file
+            with open("feedback.txt", "r") as file:
+                print(file.read())
+            with open("feedback.txt", "a") as file:
+                file.write("\n" + stuff)
+            break
 
