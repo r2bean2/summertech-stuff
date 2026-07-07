26684b878052b08ca63cda0a7f658ecfb179c12d
r2_bean2 <deanwm30@gmail.com>
2026-07-07 11:10:18 -0400

    who cares

--- evolve0.py
+++ evolve0.py
@@ -1,5 +1,9 @@
 from random import *
 import os
+import subprocess
+import ast
+import pyflakes
+import io
 if not os.path.exists("index.txt"):
     with open("index.txt", "w") as file:
         file.write("1")
@@ -10,14 +14,29 @@ else:
 print(index)
 old_index= index-1
 file_path="evolve"+str(index)+".py"
-vaild_charecters = [ chr(i) for i in range(32, 127)]
-print(vaild_charecters)
-char_add = vaild_charecters.pop(randint(0,len(vaild_charecters)))
-print(char_add)
-with open("evolve" + str(old_index) + ".py", "r") as file:
-    py_file=file.read()
-cut_point=randint(0,len(py_file))
-py_file = py_file[:cut_point] + char_add + py_file[cut_point:]
-print(py_file)
-with open("index.txt", "w") as file:
-    file.write(str(index + 1 ))
+while True:
+    vaild_charecters = [ chr(i) for i in range(32, 127)]
+    char_add = vaild_charecters.pop(randint(0,len(vaild_charecters)))
+    print(char_add)
+    with open("evolve" + str(old_index) + ".py", "r") as file:
+        py_file=file.read()
+    cut_point=randint(0,len(py_file))
+    py_file = py_file[:cut_point] + char_add + py_file[cut_point:]
+    print(py_file)
+    try:
+        ast.parse(py_file)
+        break
+    except SyntaxError:
+        continue
+    
+    with open("evolve" + str(index) + ".py", "w") as file:
+        file.write(py_file)
+    
+
+
+
+
+
+
+    with open("index.txt", "w") as file:
+        file.write(str(index + 1 ))
