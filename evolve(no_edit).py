d6e27e2e732e465b9af8bd79a3cc3f64c42409d4
r2_bean2 <deanwm30@gmail.com>
2026-07-07 12:50:23 -0400

    why are there so many songs about rainbows

--- evolve(no_edit).py
+++ evolve(no_edit).py
@@ -1,20 +1,61 @@
-from random import *
+from random import randint
 import os
+import subprocess
+import ast
+import io
+from pyflakes import api
+from pyflakes import reporter
+def check_vaild(code_string):
+    warning_stream = io.StringIO()
+    error_stream = io.StringIO()
+    rep = reporter.Reporter(warning_stream, error_stream)
+    api.check(code_string, "<mutation_string>", rep)
+    syntax_errors = error_stream.getvalue()
+    name_warnings = warning_stream.getvalue()
+    print(name_warnings)
+    print(syntax_errors)
+    # 1. Catch absolute structural failures (SyntaxError)
+    if syntax_errors:
+        return False
+        
+    # 2. Catch typos like 'randi*nt' (UndefinedName)
+    elif "undefined name" in name_warnings:
+        return False
+        
+    else:
+        return True
 if not os.path.exists("index.txt"):
     with open("index.txt", "w") as file:
-        file.write("0")
-    index=0
+        file.write("1")
+    index=1
 else:
     with open("index.txt", "r") as file:
         index=int(file.read())
 print(index)
 old_index= index-1
 file_path="evolve"+str(index)+".py"
-vaild_charecters = [ chr(i) for i in range(32, 127)]
-print(vaild_charecters)
-char_add = vaild_charecters.pop(randint(0,len(vaild_charecters)))
-print(char_add)
-with open("evolve" + str(old_index) + ".py", "r") as file:
-    py_file=file.read()
-py_file.insert(randint(0,len(py_file)),char_add)
-print(py_file)
+while True:
+    vaild_charecters = [ chr(i) for i in range(32, 127)]
+    char_add = vaild_charecters.pop(randint(0,len(vaild_charecters)-1))
+    print(char_add)
+    with open("evolve" + str(old_index) + ".py", "r") as file:
+        py_file=file.read()
+    cut_point=randint(0,len(py_file))
+    py_file = py_file[:cut_point] + char_add + py_file[cut_point:]
+    try:
+        ast.parse(py_file)
+    except SyntaxError:
+        continue
+    if check_vaild(py_file):
+        break
+with open("evolve" + str(index) + ".py", "w") as file:
+    file.write(py_file)
+    
+
+
+
+
+
+
+with open("index.txt", "w") as file:
+    file.write(str(index + 1 ))
--- evolve0.py
+++ evolve0.py
@@ -1,9 +1,29 @@
-from random import *
+from random import randint
 import os
 import subprocess
 import ast
-import pyflakes
 import io
+from pyflakes import api
+from pyflakes import reporter
+def check_vaild(code_string):
+    warning_stream = io.StringIO()
+    error_stream = io.StringIO()
+    rep = reporter.Reporter(warning_stream, error_stream)
+    api.check(code_string, "<mutation_string>", rep)
+    syntax_errors = error_stream.getvalue()
+    name_warnings = warning_stream.getvalue()
+    print(name_warnings)
+    print(syntax_errors)
+    # 1. Catch absolute structural failures (SyntaxError)
+    if syntax_errors:
+        return False
+        
+    # 2. Catch typos like 'randi*nt' (UndefinedName)
+    elif "undefined name" in name_warnings:
+        return False
+        
+    else:
+        return True
 if not os.path.exists("index.txt"):
     with open("index.txt", "w") as file:
         file.write("1")
@@ -16,27 +36,26 @@ old_index= index-1
 file_path="evolve"+str(index)+".py"
 while True:
     vaild_charecters = [ chr(i) for i in range(32, 127)]
-    char_add = vaild_charecters.pop(randint(0,len(vaild_charecters)))
+    char_add = vaild_charecters.pop(randint(0,len(vaild_charecters)-1))
     print(char_add)
     with open("evolve" + str(old_index) + ".py", "r") as file:
         py_file=file.read()
     cut_point=randint(0,len(py_file))
     py_file = py_file[:cut_point] + char_add + py_file[cut_point:]
-    print(py_file)
     try:
         ast.parse(py_file)
-        break
     except SyntaxError:
         continue
+    if check_vaild(py_file):
+        break
+with open("evolve" + str(index) + ".py", "w") as file:
+    file.write(py_file)
     
-    with open("evolve" + str(index) + ".py", "w") as file:
-        file.write(py_file)
-    
 
 
 
 
 
 
-    with open("index.txt", "w") as file:
-        file.write(str(index + 1 ))
+with open("index.txt", "w") as file:
+    file.write(str(index + 1 ))
