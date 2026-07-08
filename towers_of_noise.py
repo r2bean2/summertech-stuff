da1642228dae49b52f8b7684a7ea954642ebcf72
r2_bean2 <deanwm30@gmail.com>
2026-07-08 12:31:20 -0400

    mjjbhgghcbb

--- towers_of_noise.py
+++ towers_of_noise.py
@@ -1,8 +1,15 @@
+num_times_solved = 0
 def solve(num_disks, start, middle, end):
+    global num_times_solved
     if num_disks == 0:
         return
     else:
         num_disks_to_solve = num_disks -1
         solve(num_disks_to_solve,start,end,middle)
-        pinrt("move top disk from" + start + "to" + "end ")
+        print("move top disk " + str(start) + " to " + str(end))
+        print("recursion layer: " + str(num_disks))
+        num_times_solved += 1
+        print(num_times_solved)
+        solve(num_disks_to_solve, middle, start, end )        
 
+solve(int(input("number: ")),1,2,3)
